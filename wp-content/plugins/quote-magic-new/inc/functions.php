2b7eb4ba24ba08d0e8d00d8151ac1ba7936c140d
lescedresmarali <lescedresmarali@spilotro.dreamhost.com>
2014-02-17 22:32:26 -0800

    added some code to calculate price based on distance with the big table

--- wp-content/plugins/quote-magic-new/inc/functions.php
+++ wp-content/plugins/quote-magic-new/inc/functions.php
@@ -26,31 +26,36 @@ function quote_magic_function() {
         <!-- Row without fancy services -->
         <tr>
         <td rowspan="6">Cèdres seulement</td>
-        <td><div id="quantite_de_cedres"></div> </td>
+        <!-- First row -->
+        <td><div id="quantite_de_cedres_2_1_5"></div></td>
         <td>Hauteur 2 pieds espacement 1.5 pied</td>
-        <td><div id="unit_price"></td>
-        <td><div id="price_without_installation"></td>
-        <td rowspan="2"><div id="photo_url_small"></div></td>
+        <td><div id="unit_price_2_1_5"></div></td>
+        <td><div id="total_price_without_installation_2_1_5"></div></td>
+        <td rowspan="2"><img src="http://www.lescedresmarali.com/wp-content/uploads/2013/04/cedres-2-a-3-pieds-au-pied-et-demi.jpg" width="100%" height="100%"></div></td>
         </tr>
+        <!-- second row -->
         <tr>
-        <td><div id="quantite_de_cedres"></div> </td>
+        <td><div id="quantite_de_cedres_3_1_5"></div></td>
         <td>Hauteur 3 pieds espacement 1.5 pied</td>
         <td><div id="unit_price"></td>
-        <td><div id="price_without_installation"></td>
+        <td><div id="price_without_installation_2_1_5"></td>
         </tr>
+        <!-- third row -->
         <tr>
         <td><div id="quantite_de_cedres"></div> </td>
         <td>Hauteur 2 pieds espacement 2 pied</td>
         <td><div id="unit_price"></td>
         <td><div id="price_without_installation"></td>
-        <td rowspan="2"><div id="photo_url"></div></td>
+        <td rowspan="2"><div id="photo_url_abc"></div></td>
         </tr>
+        <!-- fourth row -->
         <tr>
         <td><div id="quantite_de_cedres"></div> </td>
         <td>Hauteur 3 pieds espacement 2 pied</td>
         <td><div id="unit_price"></td>
         <td><div id="price_without_installation"></td>
         </tr>
+        <!-- fith row -->
         <tr>
         <td><div id="quantite_de_cedres"></div> </td>
         <td>Hauteur 3 pieds espacement 2 pied</td>
@@ -58,6 +63,7 @@ function quote_magic_function() {
         <td><div id="price_without_installation"></td>
         <td rowspan="2"><div id="photo_url"></div></td>
         </tr>
+        <!-- sixth row -->
         <tr>
         <td><div id="quantite_de_cedres"></div> </td>
         <td>Hauteur 4 pieds espacement 2 pied</td>
@@ -82,7 +88,7 @@ function quote_magic_function() {
         <tr>
         <td><div id="quantite_de_cedres"></div> </td>
         <td>Hauteur 2 pieds espacement 2 pied</td>
-        <td><div id="unit_price"></td>
+        <td><div id="unit_price_2_1_5"></td>
         <td><div id="price_without_installation"></td>
         <td rowspan="2"><div id="photo_url"></div></td>
         </tr>
--- wp-content/plugins/quote-magic-new/quote-magic.js
+++ wp-content/plugins/quote-magic-new/quote-magic.js
@@ -1,112 +1,76 @@
 /**
  * 
  */
-function getRandom() {
-	var my_num = Math.floor(Math.random() * 5);
-	jQuery("#price_total").append(my_num);
-}
-
 function updateQuote() {
-	var length_in_feet;
-	var distance_between_trees;
-	var unit_price;
-	var installation_price;
-	var number_of_trees;
-	var number_of_trees_to_plant;
-	var price_without_installation;
-	var price_with_installation;
-	var installation_cost;
-	var total_price;
-	var description;
-	var photo_url;
-	var photo_url_small;
-	var photo_url_medium;
-	var photo_url_large;
-     
+    var length_in_feet;
+    var distance_between_trees;
+    var unit_price;
+    var unit_price_2_1_5=3.99;
+    var installation_price;
+    var number_of_trees;
+    var number_of_trees_to_plant;
+    var price_without_installation;
+    var price_with_installation;
+    var installation_cost;
+    var total_price;
+    var description;
+    var photo_url;
+    var photo_url_small;
+    var photo_url_medium;
+    var photo_url_large;
+    var distance_between_trees_1_5=1.5;
+    var distance_between_trees_2=2;
 
-	// This piece of code check if the user wants to have
-	// unit in Meters or in Foot.
-	if (jQuery("input[name=distance_unit]:checked").val() == 'M') {
-		length_in_feet = jQuery("#distance").val() * 3.2808399;
-	} else if (jQuery("input[name=distance_unit]:checked").val() == 'F') {
-		length_in_feet = jQuery("#distance").val();
-	}
 
-	// This piece of code check what option the customer wants
-	// option 1 = ceder
+    // This piece of code check if the user wants to have
+    // unit in Meters or in Foot.
+    if (jQuery("input[name=distance_unit]:checked").val() == 'M') {
+        length_in_feet = jQuery("#distance").val() * 3.2808399;
+    } else if (jQuery("input[name=distance_unit]:checked").val() == 'F') {
+        length_in_feet = jQuery("#distance").val();
+    }
 
-	if (jQuery("input[name=option_cedres]:checked").val() == '1') {
-		distance_between_trees=1.5;
-		unit_price=4;
-		if(length_in_feet<50){
-			installation_price=18;
-		}else{
-			installation_price=16;
-		}
-		description="Cèdres hauteur 2 à 3 pieds. Distance 1.5 pied";
-		photo_url='<img src="http://www.lescedresmarali.com/wp-content/uploads/2013/04/cedres-2-a-3-pieds-au-pied-et-demi.jpg" width="100%" height="100%">';
+    number_of_trees_2_1_5 = Math.ceil(length_in_feet/distance_between_trees_1_5);
+    number_of_trees_3_1_5 = Math.ceil(length_in_feet/distance_between_trees_1_5);
+    total_price_without_installation_2_1_5 = (number_of_trees_2_1_5 * unit_price_2_1_5).toFixed(2);
+    price_without_installation = (number_of_trees * unit_price).toFixed(2);
+    installation_cost = (number_of_trees * installation_price).toFixed(2);
+    price_with_installation = ((number_of_trees * unit_price)+(number_of_trees * installation_price)).toFixed(2);
 
-	} else if (jQuery("input[name=option_cedres]:checked").val() == '2') {
-		distance_between_trees=2;
-		unit_price=4;
-		if(length_in_feet<50){
-			installation_price=18;
-		}else{
-			installation_price=16;
-		}
-		description="Cèdres hauteur 2 à 3 pieds. Distance 2 pieds";
-		photo_url='<img src="http://www.lescedresmarali.com/wp-content/uploads/2013/04/cedres-2-a-3-pieds-au-deux-pieds.jpg" width="100%" height="100%">';
-
-	} else if (jQuery("input[name=option_cedres]:checked").val() == '3') {
-		distance_between_trees=2;
-		if(length_in_feet<50){
-			installation_price=35;
-		}else{
-			installation_price=32;
-		}
-		unit_price=12.50;
-		description="Cèdres hauteur 3 à 4 pieds. Distance 2 pieds";
-		photo_url='<img src="http://www.lescedresmarali.com/wp-content/uploads/2013/04/cedres-4-pieds-au-deux-pieds.jpg"width="100%" height="100%">';
-	}
-	
-	number_of_trees = Math.ceil(length_in_feet/distance_between_trees);
-	total_price = (number_of_trees * unit_price).toFixed(2);
-	price_without_installation = (number_of_trees * unit_price).toFixed(2);
-	installation_cost = (number_of_trees * installation_price).toFixed(2);
-	price_with_installation = ((number_of_trees * unit_price)+(number_of_trees * installation_price)).toFixed(2);
-	
-	number_of_trees_to_plant = number_of_trees;
-    number_of_trees = 10;
+    number_of_trees_to_plant = number_of_trees;
     photo_url_small = '<img src="http://www.lescedresmarali.com/wp-content/uploads/2013/04/cedres-2-a-3-pieds-au-pied-et-demi.jpg" width="100%" height="100%">';
-	jQuery("#quantite_de_cedres").text(number_of_trees);
-	jQuery("#description").text(description);
-	jQuery("#unit_price").text(unit_price+" $ ");
-	jQuery("#price_without_installation").text(price_without_installation+" $ ");
-	jQuery("#number_of_trees_to_plant").text(number_of_trees_to_plant);
-	jQuery("#installation_price").text(installation_price+" $ ");
-	jQuery("#installation_cost").text(installation_cost+" $ ");
-	jQuery("#price_with_installation").text(price_with_installation+" $");
-	jQuery("#photo_url").prepend(photo_url);
-	
-	// alert($("input[name=distance_unit]:checked").val()); 
+    jQuery("#quantite_de_cedres_2_1_5").text(number_of_trees_2_1_5);
+    jQuery("#quantite_de_cedres_3_1_5").text(number_of_trees);
+    jQuery("#description").text(description);
+    jQuery("#unit_price_2_1_5").text(unit_price_2_1_5+"$ ");
+    jQuery("#total_price_without_installation_2_1_5").text(total_price_without_installation_2_1_5+"$ ");
+    
+    jQuery("#price_without_installation").text(price_without_installation+"$ ");
+    jQuery("#price_without_installation_2_1_5").text(total_price_2_1_5+"$ ");
+    jQuery("#number_of_trees_to_plant").text(number_of_trees_to_plant);
+    jQuery("#installation_price").text(installation_price+" $ ");
+    jQuery("#installation_cost").text(installation_cost+" $ ");
+    jQuery("#price_with_installation").text(price_with_installation+" $");
+    jQuery("#photo_url").prepend(photo_url_small);
+
 }
 
 
- jQuery().ready(function() {
-	updateQuote();
-	jQuery("#distance").keyup(function(event) {
-	jQuery("#photo_url").empty();
-	updateQuote();
-	});
-	
-	jQuery("input[name=distance_unit]").click(function(event) {
-		jQuery("#photo_url").empty();
-		updateQuote();
-	});
-	
-	jQuery("input[name=option_cedres]").click(function(event) {
-		jQuery("#photo_url").empty();
-		updateQuote();
-	});
+jQuery().ready(function() {
+        updateQuote();
+        jQuery("#distance").keyup(function(event) {
+        jQuery("#photo_url").empty();
+        updateQuote();
+        });
+
+        jQuery("input[name=distance_unit]").click(function(event) {
+        jQuery("#photo_url").empty();
+        updateQuote();
+        });
+
+        jQuery("input[name=option_cedres]").click(function(event) {
+        jQuery("#photo_url").empty();
+        updateQuote();
+        });
 
 }); // End document.ready()
